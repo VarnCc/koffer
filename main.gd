234f3ad1409e6e929b16a626e3aa94f1ebb5100f
VarnCc <kevin.s.96@hotmail.de>
2026-01-26 23:40:09 +0100

    MainSetup

---
+++ main.gd
@@ -0,0 +1,16 @@
+extends Node2D
+var score = 0
+var combo = 1
+var target_points = 10.0
+var game_state = true
+var shot_active = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
