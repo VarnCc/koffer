8d7140861cfc3416501c59794bde799d0c2de51f
VarnCc <kevin.s.96@hotmail.de>
2026-01-28 00:12:07 +0100

    added out of view and bodygroup GroundBody

--- projectile.gd
+++ projectile.gd
@@ -6,8 +6,12 @@ func _physics_process(delta: float) -> void:
 	var screen_size = get_viewport_rect().size
 	
 	if position.x < 0 or position.x > screen_size.x +20:
-		print("Out of view")
+		print("Outof view")
 		queue_free()
+		emit_signal("Out_of_View")
+		if finished:
+			return
+		finished = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,8 +22,9 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node) -> void:
-	if finished:
-		return
-	finished = true
-	print("Miss (hit body): ", body.name)
-	queue_free()
+	if body.is_in_group("GroundBody"):
+		if finished:
+			return
+		finished = true
+		print("Miss (hit body): ", body.name)
+		queue_free()
