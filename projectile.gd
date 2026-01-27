8ddccbc0c636cccdd65b7cb67fc64bcc87390326
VarnCc <kevin.s.96@hotmail.de>
2026-01-28 00:22:07 +0100

    Update Projectile Skript

--- projectile.gd
+++ projectile.gd
@@ -1,5 +1,14 @@
 extends RigidBody2D
+
 var finished := false
+signal hit
+signal miss
+signal out_of_view
+
+func final():
+	if finished:
+		return
+	finished = true
 
 func _physics_process(delta: float) -> void:
 	rotation = linear_velocity.angle()
@@ -7,11 +16,9 @@ func _physics_process(delta: float) -> void:
 	
 	if position.x < 0 or position.x > screen_size.x +20:
 		print("Outof view")
+		final()
+		emit_signal("out_of_view")
 		queue_free()
-		emit_signal("Out_of_View")
-		if finished:
-			return
-		finished = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,8 +30,13 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("GroundBody"):
-		if finished:
-			return
-		finished = true
+		final()
 		print("Miss (hit body): ", body.name)
+		emit_signal("miss")
+		queue_free()
+		
+	if body.is_in_group("Target"):
+		final()
+		print("hit")
+		emit_signal("hit")
 		queue_free()
