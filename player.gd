8e4b4e21e6a4737ef47e89e68a1d09367023c571
VarnCc <kevin.s.96@hotmail.de>
2026-01-28 01:45:52 +0100

    Update Player (AimPendel + Lock)

--- player.gd
+++ player.gd
@@ -3,23 +3,26 @@ extends Node2D
 var aim_value = 0.0
 var aim_direction = 1.0
 var aim_speed = 1.5
+var aim_locked := false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	aim()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-func aim():
-	var run = true
-	while run:
-		aim_value += aim_direction * aim_speed * get_process_delta_time()
-		print(aim_value)
-		if aim_direction <= 0.00:
-			aim_direction + 0.02
-		if aim_direction >= 1.00:
-			aim_direction - 0.02
+	if Input.is_action_just_pressed("shoot"):
+		aim_locked = true
+	
+	if not aim_locked:
+		aim_value += aim_direction * aim_speed * delta
+		if aim_value >= 1.0:
+			aim_value = 1.0
+			aim_direction = -1.0
+		elif aim_value <= 0.0:
+			aim_value = 0.0
+			aim_direction = 1.0
+		
+	print(aim_value)
